1f6e5d104602b17c0ee3e7db8e852116e506d017
Anas <anashssn@gmail.com>
2023-09-15 19:09:08 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')  # Placeholder, you'd need to create this template
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')  # Placeholder, you'd need to create this template
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')  # Placeholder, you'd need to create this template
+
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')  # Placeholder, you'd need to create this template
+
+if __name__ == '__main__':
+    app.run(debug=True)
