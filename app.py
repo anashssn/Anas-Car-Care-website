30c54cfdf194df410963cc1f71e0d4defa5b0f08
Anas <anashssn@gmail.com>
2023-09-15 20:39:28 +0500

    my website for anas car care

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/services')
 def services():
